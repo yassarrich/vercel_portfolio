ad7ccd057c4ebce384c493a869853a9e15fc417e
yassarrich <yassarrich@gmail.com>
2023-09-18 16:44:15 -0400

    package and styling changes

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -29,7 +29,7 @@ export default function Contact() {
 
   return (
     <section id="contact" className="relative">
-      <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
+      <div className="container px-5 pt-5 pb-10 mx-auto flex sm:flex-nowrap flex-wrap">
         <div className="lg:w-2/3 bg-gray-800 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
 
           <img
