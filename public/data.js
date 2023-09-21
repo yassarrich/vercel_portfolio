744336aecd70144559c153c95d2c457986437f15
yassarrich <yassarrich@gmail.com>
2023-09-21 09:45:31 -0400

    styling changes

--- public/data.js
+++ public/data.js
@@ -1,38 +1,37 @@
 // src/data.js
 
 export const projects = [
+      {
+        title: " User and Password Manager",
+        status:"Completed",
+        subtitle: "C++",
+        description:
+          "This project implements a abstract hash table data type, this program is a secure password server and provides user interface in order to manage the different users and password.",
+        image: "https://images.pexels.com/photos/276452/pexels-photo-276452.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        link: "https://github.com/yassarrich/passwordManager",
+        github: "https://github.com/yassarrich/passwordManager",
+        tech:["C++", "Nano", "Git"] 
+      },
     {
       title: "studyti.me",
-      status:"In Progress",
+      status:"In Progress - Database no longer supported",
       subtitle: "React Application",
       description:
-        "Study tool meant to keep track of todos and displays a Pomodoro Timer in order to keep track of work done in sprints",
+        "Study tool meant to keep track of todos and displays a Pomodoro Timer in order to keep track of work done",
       image: "https://c4.wallpaperflare.com/wallpaper/600/919/630/digital-art-illustration-lofi-hd-wallpaper-preview.jpg",
       link: "https://studytimev1.vercel.app/",
       github: "https://github.com/yassarrich/studytime-version1",
-      tech: ["React", "Vercel", "JavaScript"]
+      tech: ["React", "Vercel", "Firebase"]
     },
     {
       title: "Python Tech News App",
-      status:"In Progress",
+      status:"In Progress - currently migrating resources",
       subtitle: "HTML CSS and Python",
       description:
-        "Full Stack web app hosted on a Ubuntu Server using Nginx. This project is my introduction to the software development life cycle, Currently working on adding React frontend to display news article more aesthetically",
+        "Full Stack web app hosted on a Ubuntu Server using Nginx. This project is my introduction to the software development life cycle, Currently working on adding React frontend to display news article more aesthetically, under maintencance",
       image: "https://images.ctfassets.net/lzny33ho1g45/1myGI9Ws1d0hbffMs6BcnD/01229f11aae0fc021bc8a8eb0c28ba52/Best_news_apps_hero.png?w=1520&fm=jpg&q=30&fit=thumb&h=760",
       link: "https://pytech-news.herokuapp.com/",
       github: "https://github.com/yassarrich/pytech.news",
-      tech: ["Python", "Nginx", "HTML", "CSS"]
-    },
-    {
-      title: " User and Password Manager",
-      status:"Completed",
-      subtitle: "C++",
-      description:
-        "This project implements a hash table abstract data type, this program is a secure password server and provides user interface in order to manage the different users and password.",
-      image: "https://images.pexels.com/photos/276452/pexels-photo-276452.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      link: "https://github.com/yassarrich/passwordManager",
-      github: "https://github.com/yassarrich/passwordManager",
-      tech:["C++", "Hash Table", "Security"] 
-    },
-
+      tech: ["Python", "React", "Tailwind"]
+    }
   ];
