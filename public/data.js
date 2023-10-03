77988bb47a99713c73550016dc5218fc06d69f7a
yassarrich <yassarrich@gmail.com>
2023-10-03 17:54:53 -0400

    Hid incomplete projects

--- public/data.js
+++ public/data.js
@@ -11,27 +11,28 @@ export const projects = [
         link: "https://github.com/yassarrich/passwordManager",
         github: "https://github.com/yassarrich/passwordManager",
         tech:["C++", "Nano", "Git"] 
-      },
-    {
-      title: "studyti.me",
-      status:"In Progress - Database no longer supported",
-      subtitle: "React Application",
-      description:
-        "Study tool meant to keep track of todos and displays a Pomodoro Timer in order to keep track of work done",
-      image: "https://c4.wallpaperflare.com/wallpaper/600/919/630/digital-art-illustration-lofi-hd-wallpaper-preview.jpg",
-      link: "https://studytimev1.vercel.app/",
-      github: "https://github.com/yassarrich/studytime-version1",
-      tech: ["React", "Vercel", "Firebase"]
-    },
-    {
-      title: "Python Tech News App",
-      status:"In Progress - currently migrating resources",
-      subtitle: "HTML CSS and Python",
-      description:
-        "Full Stack web app hosted on a Ubuntu Server using Nginx. This project is my introduction to the software development life cycle, Currently working on adding React frontend to display news article more aesthetically, under maintencance",
-      image: "https://images.ctfassets.net/lzny33ho1g45/1myGI9Ws1d0hbffMs6BcnD/01229f11aae0fc021bc8a8eb0c28ba52/Best_news_apps_hero.png?w=1520&fm=jpg&q=30&fit=thumb&h=760",
-      link: "https://pytech-news.herokuapp.com/",
-      github: "https://github.com/yassarrich/pytech.news",
-      tech: ["Python", "React", "Tailwind"]
-    }
+      }
+    //   },
+    // {
+    //   title: "studyti.me",
+    //   status:"In Progress - Database no longer supported",
+    //   subtitle: "React Application",
+    //   description:
+    //     "Study tool meant to keep track of todos and displays a Pomodoro Timer in order to keep track of work done",
+    //   image: "https://c4.wallpaperflare.com/wallpaper/600/919/630/digital-art-illustration-lofi-hd-wallpaper-preview.jpg",
+    //   link: "https://studytimev1.vercel.app/",
+    //   github: "https://github.com/yassarrich/studytime-version1",
+    //   tech: ["React", "Vercel", "Firebase"]
+    // },
+    // {
+    //   title: "Python Tech News App",
+    //   status:"In Progress - currently migrating resources",
+    //   subtitle: "HTML CSS and Python",
+    //   description:
+    //     "Full Stack web app hosted on a Ubuntu Server using Nginx. This project is my introduction to the software development life cycle, Currently working on adding React frontend to display news article more aesthetically, under maintencance",
+    //   image: "https://images.ctfassets.net/lzny33ho1g45/1myGI9Ws1d0hbffMs6BcnD/01229f11aae0fc021bc8a8eb0c28ba52/Best_news_apps_hero.png?w=1520&fm=jpg&q=30&fit=thumb&h=760",
+    //   link: "https://pytech-news.herokuapp.com/",
+    //   github: "https://github.com/yassarrich/pytech.news",
+    //   tech: ["Python", "React", "Tailwind"]
+    // }
   ];
